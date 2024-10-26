3d4a8360b40f521487b898e3b943b4ae597c3bad
Daniel Ceseñas <cesenasdaniel@outlook.com>
2024-10-26 13:48:40 -0600

    Segundo commit

--- script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    // Añade la clase correspondiente para activar la animación
-    document.querySelector(".progress.html").style.width = "50%";
-    document.querySelector(".progress.adobe").style.width = "40%";
-    document.querySelector(".progress.SQL").style.width = "40%";
-    document.querySelector(".progress.com").style.width = "90%";
-    document.querySelector(".progress.teamwork").style.width = "70%";
-    document.querySelector(".progress.dedication").style.width = "90%";
-});
